63b1389a5c4e6d0bf789341e5b7d35583dcbb14d
Sarim Zafar <sarimzafar22@gmail.com>
2020-09-19 13:55:53 +0500

    Updates

---
+++ precache-manifest.5a2b3f458c1167b6f2bb35fe922af8e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeaeb07b80e8f1b7751feb6483171cc8",
+    "url": "/react-landing-page/index.html"
+  },
+  {
+    "revision": "f2fe82e3fe96aeacd3be",
+    "url": "/react-landing-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d668754a3f0cd60b2390",
+    "url": "/react-landing-page/static/js/2.94a865ff.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-landing-page/static/js/2.94a865ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2fe82e3fe96aeacd3be",
+    "url": "/react-landing-page/static/js/main.18db9f35.chunk.js"
+  },
+  {
+    "revision": "7c00c3f9a07628639262",
+    "url": "/react-landing-page/static/js/runtime-main.13ea5871.js"
+  }
+]);
